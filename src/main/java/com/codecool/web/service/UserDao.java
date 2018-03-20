372f6d0d3da51e97e5d96029488741e18482655e
GaborMartin <gabormartin94@gmail.com>
2018-03-20 12:04:25 +0100

    Updated method signatures

--- src/main/java/com/codecool/web/service/UserDao.java
+++ src/main/java/com/codecool/web/service/UserDao.java
@@ -11,6 +11,10 @@ public interface UserDao {
     public void addNewUser(User newUser);
     public boolean isEmailExists(String email);
     public User getUserByEmail(String email);
-    public void updateUser(User user, String toUpdate, Object newData);
     public void deleteUser(User user);
+    public void updateBrith(User user, Object newData);
+    public void updateGender(User user, Object newData);
+    public void updatePassword(User user, Object newData);
+    public void updateEmail(User user, Object newData);
+    public void updateName(User user, Object newName);
 }
