ac92f140249660ba087b3ef9788206e04b7a3be0
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-03-20 13:05:50 +0100

    Register servlet implemented,checks for empty inputs,existing user,adds new user to users if registration was valid

---
+++ src/main/java/com/codecool/web/servlet/RegisterServlet.java
@@ -0,0 +1,36 @@
+package com.codecool.web.servlet;
+
+import com.codecool.web.model.User;
+import com.codecool.web.service.UserDaoImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String role = req.getParameter("role");
+        String password = req.getParameter("password");
+        if (name.equals("") || email.equals("") || role.equals("") || password.equals("")) {
+            return;
+        }
+
+        User user = new User(name,email,role,password);
+
+        UserDaoImpl userDao = new UserDaoImpl();
+        if(userDao.isEmailExists(email)){
+            return;
+        }
+        userDao.addNewUser(user);
+        resp.sendRedirect("login.jsp");
+        
+    }
+}
