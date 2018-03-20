471c42d9ddfe5d68eb55033b0973e6513a75e809
Bernáth Máté <bernath.mate.new@gmail.com>
2018-03-20 11:53:39 +0100

    addNewUser method is added, updateUser's method signature is modified

--- src/main/java/com/codecool/web/service/UserDao.java
+++ src/main/java/com/codecool/web/service/UserDao.java
@@ -8,8 +8,9 @@ import java.util.List;
 public interface UserDao {
 
     public List<User> getAllUsers();
+    public void addNewUser(User newUser);
     public boolean isEmailExists(String email);
     public User getUserByEmail(String email);
-    public void updateUser(User user);
+    public void updateUser(User user, String toUpdate, Object newData);
     public void deleteUser(User user);
 }
