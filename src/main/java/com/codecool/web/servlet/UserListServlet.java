2ab1735b27fa46c5c0e5ce1c176115d4ec634df6
Bernáth Máté <bernath.mate.new@gmail.com>
2018-03-21 13:45:23 +0100

    UserListServlet is created, doGet() method is implemented

---
+++ src/main/java/com/codecool/web/servlet/UserListServlet.java
@@ -0,0 +1,25 @@
+package com.codecool.web.servlet;
+
+import com.codecool.web.model.User;
+import com.codecool.web.service.UserDaoImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/userlist")
+public class UserListServlet extends HttpServlet {
+
+    private UserDaoImpl udi = new UserDaoImpl();
+    private List<User> users = udi.getAllUsers();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("users", users);
+        req.getRequestDispatcher("userlist.jsp").forward(req, resp);
+    }
+}
