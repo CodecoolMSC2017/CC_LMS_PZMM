224d5a76f0590d1d88fafe0b3fdfba9c5ca50747
GaborMartin <gabormartin94@gmail.com>
2018-03-22 10:29:57 +0100

    Implemented CurriculumList class as singleton

---
+++ src/main/java/com/codecool/web/model/CurriculumList.java
@@ -0,0 +1,19 @@
+package com.codecool.web.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CurriculumList {
+    private List<Curriculum> curriculums;
+    
+    private CurriculumList() {
+        curriculums = new ArrayList<>();
+    }
+
+    private static CurriculumList curriculumList = new CurriculumList();
+
+    public static CurriculumList getInstance() {
+        return curriculumList;
+    }
+
+}
