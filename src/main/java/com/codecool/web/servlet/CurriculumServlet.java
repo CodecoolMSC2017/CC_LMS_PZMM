70e7a36e01efef2a3feb1e63e5491392b19b7612
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-03-22 12:29:07 +0100

    minor modifications

--- src/main/java/com/codecool/web/servlet/CurriculumServlet.java
+++ src/main/java/com/codecool/web/servlet/CurriculumServlet.java
@@ -26,16 +26,13 @@ public class CurriculumServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession sess = req.getSession();
-        Object user1 = sess.getAttribute("loggedUser");
-        User user = (User) user1;
+        User user =(User) sess.getAttribute("loggedUser");
         curriculums.add(new Curriculum("fasz","fityma",true));
         curriculums.add(new Curriculum("fasz","fityma",false));
-
 
         if (user.getRole().equals("student")) {
             req.setAttribute("curriculumLink","/curriculum");
             req.setAttribute("curriculums",filterList());
-
 
         }
         else {
@@ -43,11 +40,6 @@ public class CurriculumServlet extends HttpServlet{
             req.setAttribute("curriculums",curriculums);
 
         }
-
-
-        req.setAttribute("curriculumLink","/curriculumEdit");
-        req.setAttribute("curriculums",curriculums);
-
 
         resp.sendRedirect("index.jsp");
     }
