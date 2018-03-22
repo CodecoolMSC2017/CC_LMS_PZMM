3c2b489921ea341a6bee84826022769681b5858b
peti120 <peti59550@gmail.com>
2018-03-22 14:29:35 +0100

    Session created for the request.

--- src/main/java/com/codecool/web/servlet/SetCurriculumServlet.java
+++ src/main/java/com/codecool/web/servlet/SetCurriculumServlet.java
@@ -8,13 +8,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/setCurriculum")
 public class SetCurriculumServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User loggedInUser = (User) req.getAttribute("loggedUser");
+        HttpSession session = req.getSession();
+        User loggedInUser = (User) session.getAttribute("loggedUser");
         if (loggedInUser.getRole().equals("student")) {
             Curriculum curriculum = (Curriculum) req.getAttribute("selectedCurriculum");
             req.setAttribute("curriculum", curriculum);
@@ -24,8 +26,5 @@ public class SetCurriculumServlet extends HttpServlet {
             req.setAttribute("curriculum", curriculum);
             req.getRequestDispatcher("curriculumedit.jsp").forward(req, resp);
         }
-
-
-
     }
 }
