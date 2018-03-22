76b4e104c1c009f5b8531e63fca0dba7cc545d59
Bernáth Máté <bernath.mate.new@gmail.com>
2018-03-22 10:12:45 +0100

    CurriculumDaoImpl class's skeleton is done.

---
+++ src/main/java/com/codecool/web/service/CurriculumDaoImpl.java
@@ -0,0 +1,44 @@
+package com.codecool.web.service;
+
+public class CurriculumDaoImpl implements CurriculumDao {
+    
+    @Override
+    public List<Curriculum> getAllCurriculums() {
+        return null;
+    }
+
+    @Override
+    public void addNewCurriculum(Curriculum newCurriculum) {
+
+    }
+
+    @Override
+    public void removeCurriculum(Curriculum curriculum) {
+
+    }
+
+    @Override
+    public Curriculum getCurriculumByTitle(String title) {
+        return null;
+    }
+
+    @Override
+    public void updateCurriculumTitle(Curriculum curriculum, String newTitle) {
+
+    }
+
+    @Override
+    public void updateContent(Curriculum curriculum, String newContent) {
+
+    }
+
+    @Override
+    public void updateIsPublished(Curriculum curriculum, boolean isPublished) {
+
+    }
+
+    @Override
+    public void updateAssignment(Curriculum curriculum, Assignment newAssignment) {
+
+    }
+}
