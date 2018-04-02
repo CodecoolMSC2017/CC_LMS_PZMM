1e53bcc55f7485f1f120be1b0499e1533149f922
Gábor <gabormartin94@gmail.com>
2018-04-02 17:33:13 +0200

    Implemented basic ProfileEditorServlet

---
+++ src/main/java/com/codecool/web/servlet/ProfileEditorServlet.java
@@ -0,0 +1,39 @@
+package com.codecool.web.servlet;
+
+import com.codecool.web.model.User;
+import com.codecool.web.service.EmptyFieldException;
+import com.codecool.web.service.LoginService;
+import com.codecool.web.service.UserDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/protected/profile")
+public class ProfileEditorServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = (User) req.getSession().getAttribute("user");
+        req.setAttribute("user", user);
+        req.getRequestDispatcher("profile.jsp").forward(req, resp);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserDao userService = (UserDao) req.getServletContext().getAttribute("userService");
+        User user = (User) req.getSession().getAttribute("user");
+
+        try {
+            userService.updateName(user, req.getParameter("name"));
+        } catch (EmptyFieldException e) {
+            req.setAttribute("error", "Field name cannot be empty!");
+        }
+
+        userService.updateRole(user, req.getParameter("role"));
+
+        req.getRequestDispatcher("profileeditor.jsp").forward(req, resp);
+        resp.sendRedirect("profile.jsp");
+    }
+}
