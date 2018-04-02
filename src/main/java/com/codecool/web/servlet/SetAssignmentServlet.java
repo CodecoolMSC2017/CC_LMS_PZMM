fed4a3c8d2a5cb6e02862fe236c571e87a3ecf12
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-04-02 19:42:23 +0200

    SetAssignmentServlet added

---
+++ src/main/java/com/codecool/web/servlet/SetAssignmentServlet.java
@@ -0,0 +1,30 @@
+package com.codecool.web.servlet;
+
+import com.codecool.web.model.Assignment;
+import com.codecool.web.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/setAssignment")
+public class SetAssignmentServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        //need assignmentDao implementation
+        //AssignmentDao assignmentDao = (AssignmentDao) req.getServletContext().getAttribute("assignmentService");
+        User loggedInUser = (User) session.getAttribute("user");
+        if (loggedInUser.getRole().equals("student")) {
+            //session.setAttribute("selectedAssignment", assignmentDao.getAssignmentByTitle(req.getParameter("assignment")));
+            req.getRequestDispatcher("protected/assignment.jsp").forward(req, resp);
+        } else {
+            //session.setAttribute("selectedAssignment", assignmentDao.getAssignmentByTitle(req.getParameter("assignment")));
+            req.getRequestDispatcher("protected/assignmentEdit.jsp").forward(req, resp);
+        }
+    }
+}
