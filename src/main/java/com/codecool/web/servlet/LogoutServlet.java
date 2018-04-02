d73878d88ceb6999861dcfceeebe900006de3ba2
Gábor <gabormartin94@gmail.com>
2018-04-02 12:44:17 +0200

    Refactored logoutServlet

--- src/main/java/com/codecool/web/servlet/LogoutServlet.java
+++ src/main/java/com/codecool/web/servlet/LogoutServlet.java
@@ -11,12 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet("/logout")
+@WebServlet("/protected/logout")
 public class LogoutServlet extends HttpServlet {
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        HttpSession session = req.getSession();
-        session.setAttribute("loggedUser",null);
-        resp.sendRedirect("login.jsp");
+        req.getSession().invalidate();
+        resp.sendRedirect("..");
     }
 }
