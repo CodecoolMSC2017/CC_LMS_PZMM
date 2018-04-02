4ebcc7a66aa81ed1508db7726ed8250ca6c13601
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-04-02 19:41:24 +0200

    assignment editor servlet added

---
+++ src/main/java/com/codecool/web/servlet/AssignmentEditorServlet.java
@@ -0,0 +1,29 @@
+package com.codecool.web.servlet;
+
+import com.codecool.web.model.Assignment;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/assignmentEditor")
+public class AssignmentEditorServlet extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Assignment selectedAssignment = (Assignment) session.getAttribute("selectedAssignment");
+
+        //selectedAssignment.setAssignment(hashmap)
+        selectedAssignment.setTitle(req.getParameter("title"));
+        selectedAssignment.setMaxScore(Integer.parseInt(req.getParameter("maxScore")));
+        selectedAssignment.setPublished(Boolean.parseBoolean(req.getParameter("isPublished")));
+
+        req.setAttribute("selectedAssignment", selectedAssignment);
+        req.getRequestDispatcher("protected/assignment.jsp").forward(req, resp);
+        resp.sendRedirect("assignment.jsp");
+    }
+}
