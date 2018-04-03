6a67d0fe12f71c995db0d22f022b295a3463201e
GaborMartin <gabormartin94@gmail.com>
2018-04-03 11:34:11 +0200

    Fixed field names

--- src/main/java/com/codecool/web/servlet/CurriculumCreationServlet.java
+++ src/main/java/com/codecool/web/servlet/CurriculumCreationServlet.java
@@ -17,12 +17,12 @@ public class CurriculumCreationServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         CurriculumDao curriculumService = (CurriculumDao) req.getServletContext().getAttribute("curriculumService");
 
-        String name = req.getParameter("title");
-        String email = req.getParameter("content");
-        boolean role = Boolean.parseBoolean(req.getParameter("isPublished"));
+        String title = req.getParameter("title");
+        String content = req.getParameter("content");
+        boolean visibility = Boolean.parseBoolean(req.getParameter("isPublished"));
 
         try {
-            curriculumService.addNewCurriculum(name, email, role);
+            curriculumService.addNewCurriculum(title, content, visibility);
             req.setAttribute("info", "Curriculum creation is successful!");
         } catch (EmptyFieldException e) {
             req.setAttribute("error", "Title and content field cannot be empty!");
