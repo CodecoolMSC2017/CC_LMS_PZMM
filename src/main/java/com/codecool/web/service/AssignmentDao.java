d7cdbf70582a7fb031da0662584fb58ff15c48e8
Bernáth Máté <bernath.mate.new@gmail.com>
2018-04-03 10:55:45 +0200

    addNewAssignment method's signature is refactored

--- src/main/java/com/codecool/web/service/AssignmentDao.java
+++ src/main/java/com/codecool/web/service/AssignmentDao.java
@@ -2,12 +2,13 @@ package com.codecool.web.service;
 
 import com.codecool.web.model.Assignment;
 
+import java.util.HashMap;
 import java.util.List;
 
 public interface AssignmentDao {
 
     public List<Assignment> getAllAssignments();
-    public void addNewAssignment(Assignment newAssignment);
+    public void addNewAssignment(String question, String title, int maxScore, boolean isPublished) throws EmptyFieldException;
     public void removeAssignment(Assignment assignment);
     public Assignment getAssignmentByTitle(String title);
     public void updateAssignmentTitle(Assignment assignment, String newTitle);
