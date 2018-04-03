bd3baf088723ee9916003cc1e713193d76e97797
peti120 <peti59550@gmail.com>
2018-04-03 15:23:53 +0200

    Fixed Servlet

--- src/main/java/com/codecool/web/servlet/AssignmentEditorServlet.java
+++ src/main/java/com/codecool/web/servlet/AssignmentEditorServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet("/protected/assignmentEditorServlet")
+@WebServlet("/assignmentEditorServlet")
 public class AssignmentEditorServlet extends HttpServlet{
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
