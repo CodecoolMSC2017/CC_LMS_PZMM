9a6e779e786faa5e43d9b5f595da62cbe41e7d23
peti120 <peti59550@gmail.com>
2018-04-17 11:12:19 +0200

    CurriculumService Implemented

---
+++ src/main/java/com/codecool/web/service/CurriculumService.java
@@ -0,0 +1,15 @@
+package com.codecool.web.service;
+
+import com.codecool.web.model.Curriculum;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CurriculumService {
+    List<Curriculum> getAllCurriculums() throws SQLException;
+    Curriculum addNewCurriculum(String title, String content, boolean isPublished) throws EmptyFieldException, SQLException, ServiceException;
+    Curriculum getCurriculumByTitle(String title) throws SQLException;
+    void updateCurriculumTitle(Curriculum curriculum, String newTitle) throws EmptyFieldException, SQLException, ServiceException;
+    void updateContent(Curriculum curriculum, String newContent) throws EmptyFieldException, SQLException, ServiceException;
+    void updateIsPublished(Curriculum curriculum, boolean isPublished) throws SQLException;
+}
---
+++ src/main/java/com/codecool/web/service/SimpleCurriculumService.java
@@ -0,0 +1,55 @@
+package com.codecool.web.service;
+
+import com.codecool.web.model.Curriculum;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class SimpleCurriculumService implements CurriculumService{
+    private final CurriculumDao curriculumDao;
+
+    public SimpleCurriculumService(CurriculumDao curriculumDao) {
+        this.curriculumDao = curriculumDao;
+    }
+    @Override
+    public List<Curriculum> getAllCurriculums() throws SQLException {
+        return curriculumDao.getAllCurriculums();
+    }
+
+    @Override
+    public Curriculum addNewCurriculum(String title, String content, boolean isPublished) throws SQLException, ServiceException {
+        try {
+            return curriculumDao.addNewCurriculum(title,content,isPublished);
+        } catch (EmptyFieldException e) {
+            throw new ServiceException("Title shouldn't be an empty field!");
+        }
+    }
+
+    @Override
+    public Curriculum getCurriculumByTitle(String title) throws SQLException {
+        return curriculumDao.getCurriculumByTitle(title);
+    }
+
+    @Override
+    public void updateCurriculumTitle(Curriculum curriculum, String newTitle) throws SQLException, ServiceException {
+        try {
+            curriculumDao.updateCurriculumTitle(curriculum,newTitle);
+        } catch (EmptyFieldException e) {
+            throw new ServiceException("Title shouldn't be an empty field!");
+        }
+    }
+
+    @Override
+    public void updateContent(Curriculum curriculum, String newContent) throws SQLException, ServiceException {
+        try {
+            curriculumDao.updateContent(curriculum,newContent);
+        } catch (EmptyFieldException e) {
+            throw new ServiceException("Title shouldn't be an empty field!");
+        }
+    }
+
+    @Override
+    public void updateIsPublished(Curriculum curriculum, boolean isPublished) throws SQLException {
+        curriculumDao.updateIsPublished(curriculum,isPublished);
+    }
+}
