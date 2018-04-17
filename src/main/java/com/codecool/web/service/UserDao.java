d94385c29bc4c02a767ae0ae92fe3c585d63951d
Bernáth Máté <bernath.mate.new@gmail.com>
2018-04-17 11:03:36 +0200

    UserDao interface is refactored, findAllUsers() instead of getAllUsers() method, this method will return all users from the DB, addNewUser method will create and insert a new user to the DB, and this method will return the created user object, isEmailExists() method will looking for the given email in the DB, if this email is exist this method will return true, otherwise false, getUserByEmail() method will return a user object from the DB based on email, getUserById() method will return a user object from the DB based on email, the update methods will update the given row in the DB

--- src/main/java/com/codecool/web/service/UserDao.java
+++ src/main/java/com/codecool/web/service/UserDao.java
@@ -2,17 +2,19 @@ package com.codecool.web.service;
 
 import com.codecool.web.model.User;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface UserDao {
 
-    public List<User> getAllUsers();
-    public void addNewUser(String name, String email, String role, String password) throws InvalidRegistrationException, InvalidEmailAddressException, InvalidPasswordException, EmailAddressAlreadyExistsException;
-    public boolean isEmailExists(String email);
+    public List<User> findAllUsers() throws SQLException;
+    public User addNewUser(String name, String email, String role, String password) throws InvalidRegistrationException, InvalidEmailAddressException, InvalidPasswordException, EmailAddressAlreadyExistsException, SQLException;
+    public boolean isEmailExists(String email) throws SQLException;
     public boolean validateEmailAddress(String email);
     public boolean validatePassword(String pw);
-    public User getUserByEmail(String email);
-    public void deleteUser(User user);
-    public void updateName(User user, String newName) throws EmptyFieldException;
-    public void updateRole(User user, String newRole);
+    public User getUserByEmail(String email) throws SQLException;
+    public User getUserById(int id) throws SQLException;
+    public void updateName(int id, String name) throws EmptyFieldException, SQLException;
+    public void updateRole(int id, String name) throws SQLException;
+    public void updateNameAndRole(int id, String newName, String role) throws SQLException;
 }
