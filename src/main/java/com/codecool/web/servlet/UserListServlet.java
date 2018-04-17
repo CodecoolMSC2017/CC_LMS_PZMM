5a3b3bc6c4a90cf8d66b42fde699adba8b2eca0e
Bernáth Máté <bernath.mate.new@gmail.com>
2018-04-17 11:09:49 +0200

    Imports are refactored.

--- src/main/java/com/codecool/web/servlet/UserListServlet.java
+++ src/main/java/com/codecool/web/servlet/UserListServlet.java
@@ -1,16 +1,12 @@
 package com.codecool.web.servlet;
 
-import com.codecool.web.model.User;
 import com.codecool.web.service.UserDao;
-import com.codecool.web.service.UserDaoImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet("/protected/userlist")
 public class UserListServlet extends AbstractServlet {
