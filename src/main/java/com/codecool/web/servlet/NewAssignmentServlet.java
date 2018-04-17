b461e9f1a3707189b787846f123c23dc16a9d88d
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-04-17 13:43:46 +0200

    Servlet refactored to use Database

--- src/main/java/com/codecool/web/servlet/NewAssignmentServlet.java
+++ src/main/java/com/codecool/web/servlet/NewAssignmentServlet.java
@@ -1,17 +1,20 @@
 package com.codecool.web.servlet;
 
-import com.codecool.web.service.AssignmentDao;
-import com.codecool.web.service.EmptyFieldException;
+import com.codecool.web.service.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebServlet("/protected/newassignment")
-public class NewAssignmentServlet extends AbstractServlet{
+public class NewAssignmentServlet extends AbstractServlet {
+
+    public static final String SQL_ERROR_CODE_UNIQUE_VIOLATION = "23505";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("newassignment.jsp").forward(req, resp);
@@ -19,20 +22,31 @@ public class NewAssignmentServlet extends AbstractServlet{
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        AssignmentDao assignmentDao = (AssignmentDao) req.getServletContext().getAttribute("assignmentService");
+        try (Connection connection = getConnection(req.getServletContext())) {
 
-        String title = req.getParameter("title");
-        String question = req.getParameter("question");
-        String maxScore = req.getParameter("maxscore");
-        boolean visibility = Boolean.parseBoolean(req.getParameter("visibility"));
+            AssignmentDao assignmentDataBaseDao = new AssignmentDatabaseDao(connection);
+            AssignmentService assignmentService = new SimpleAssignmentService(assignmentDataBaseDao);
 
-        try {
-            assignmentDao.addNewAssignment(question, title, Integer.parseInt(maxScore), visibility);
-            req.setAttribute("info", "New assignment is added!");
-        } catch (EmptyFieldException e) {
-            req.setAttribute("error", "Fill title and question please!");
+            String title = req.getParameter("title");
+            String question = req.getParameter("question");
+            String maxScore = req.getParameter("maxscore");
+            boolean isPublished = Boolean.parseBoolean(req.getParameter("visibility"));
+
+
+            try {
+                assignmentService.addAssignment(title, question, Integer.parseInt(maxScore), false, isPublished);
+                req.setAttribute("info", "New assignment is added!");
+            } catch (ServiceException e) {
+                req.setAttribute("error", "Fill title and question please!");
+            }
+        } catch (SQLException ex) {
+            if (SQL_ERROR_CODE_UNIQUE_VIOLATION.equals(ex.getSQLState())) {
+                req.setAttribute("error", "Coupon has been already added to one of the selected shops");
+            } else {
+                throw new ServletException(ex);
+            }
+            req.getRequestDispatcher("newassignment.jsp").forward(req, resp);
+
         }
-        req.getRequestDispatcher("newassignment.jsp").forward(req, resp);
-
     }
 }
