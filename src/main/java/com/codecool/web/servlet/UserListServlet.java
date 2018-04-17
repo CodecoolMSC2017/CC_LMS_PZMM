137c549b360e53aa98e2d1d684a7cceec5c4545c
Bernáth Máté <bernath.mate.new@gmail.com>
2018-04-17 13:50:28 +0200

    UserListServlet is refactored, this servlet will send each user to the jsp from the DB.

--- src/main/java/com/codecool/web/servlet/UserListServlet.java
+++ src/main/java/com/codecool/web/servlet/UserListServlet.java
@@ -1,5 +1,7 @@
 package com.codecool.web.servlet;
 
+import com.codecool.web.model.User;
+import com.codecool.web.service.DatabaseUserDao;
 import com.codecool.web.service.UserDao;
 
 import javax.servlet.ServletException;
@@ -7,14 +9,24 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/protected/userlist")
 public class UserListServlet extends AbstractServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        UserDao userService = (UserDao) req.getServletContext().getAttribute("userService");
-        req.setAttribute("users", userService.getAllUsers());
-        req.getRequestDispatcher("userlist.jsp").forward(req, resp);
+        try (Connection connection = getConnection(req.getServletContext())){
+            UserDao userDao = new DatabaseUserDao(connection);
+
+            List<User> users = userDao.findAllUsers();
+
+            req.setAttribute("users", users);
+            req.getRequestDispatcher("userlist.jsp").forward(req, resp);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
