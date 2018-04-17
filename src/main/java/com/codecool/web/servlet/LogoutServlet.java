99dfa18f9f2acfb42181c94d629a9d9ae7987776
Bernáth Máté <bernath.mate.new@gmail.com>
2018-04-17 11:09:18 +0200

    Imports are refactored.

--- src/main/java/com/codecool/web/servlet/LogoutServlet.java
+++ src/main/java/com/codecool/web/servlet/LogoutServlet.java
@@ -1,14 +1,9 @@
 package com.codecool.web.servlet;
 
-import com.codecool.web.model.User;
-import com.codecool.web.service.UserDaoImpl;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/protected/logout")
