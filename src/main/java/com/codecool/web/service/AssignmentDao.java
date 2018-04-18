b18bf9a37f4c82036c543dc3321e84aacd34f6b5
Bernáth Máté <bernath.mate.new@gmail.com>
2018-04-18 13:05:07 +0200

    getSubmittedAssignmentsById method is refactored, this method will return a hashmap, getAssignmentByUserId method is removed

--- src/main/java/com/codecool/web/service/AssignmentDao.java
+++ src/main/java/com/codecool/web/service/AssignmentDao.java
@@ -16,11 +16,9 @@ public interface AssignmentDao {
     public void updateAssignmentTitleById(int id, String newTitle) throws EmptyFieldException, SQLException;
     public void updateAssignmentQuestionById(int id, String newQuestion) throws EmptyFieldException, SQLException;
     public void updateMaxScoreById(int id,int score) throws SQLException;
-    public void updateIsDoneById(int id, boolean isDone) throws SQLException;
     public void updateIsPublishedById(int id, boolean isPublished) throws SQLException;
     public void addToSubmittedAssignments(int assignmentId, int userId, String answer) throws SQLException;
     public Assignment fetchAssignment(ResultSet resultSet) throws SQLException;
-    public List<Assignment> getSubmittedAssignmentsById(int userId) throws SQLException;
+    public HashMap<Assignment, String> getSubmittedAssignmentsById(int userId) throws SQLException;
     public List<Assignment> getUnSubmittedAssignmentsById(int userId) throws SQLException;
-    public Assignment getAssignmentByUserId(int userId, int assignmentId);
 }
