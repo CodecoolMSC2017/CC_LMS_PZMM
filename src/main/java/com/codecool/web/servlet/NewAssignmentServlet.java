26b1d551ac6b93cec217e5cb42e11e769111ccaa
Bernáth Máté <bernath.mate.new@gmail.com>
2018-04-19 11:43:00 +0200

    doGet method is fixed, now it is working fine

--- src/main/java/com/codecool/web/servlet/NewAssignmentServlet.java
+++ src/main/java/com/codecool/web/servlet/NewAssignmentServlet.java
@@ -17,8 +17,6 @@ import java.sql.SQLException;
 @WebServlet("/protected/newassignment")
 public class NewAssignmentServlet extends AbstractServlet {
 
-    public static final String SQL_ERROR_CODE_UNIQUE_VIOLATION = "23505";
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("newassignment.jsp").forward(req, resp);
@@ -36,21 +34,13 @@ public class NewAssignmentServlet extends AbstractServlet {
             String maxScore = req.getParameter("maxscore");
             boolean isPublished = Boolean.parseBoolean(req.getParameter("visibility"));
 
-
-            try {
-                assignmentService.addAssignment(title, question, Integer.parseInt(maxScore), isPublished);
-                req.setAttribute("info", "New assignment is added!");
-            } catch (ServiceException e) {
-                req.setAttribute("error", "Fill title and question please!");
-            }
-        } catch (SQLException ex) {
-            if (SQL_ERROR_CODE_UNIQUE_VIOLATION.equals(ex.getSQLState())) {
-                req.setAttribute("error", "Assignment has been already added");
-            } else {
-                throw new ServletException(ex);
-            }
-            req.getRequestDispatcher("newassignment.jsp").forward(req, resp);
-
+            assignmentService.addAssignment(title, question, Integer.parseInt(maxScore), isPublished);
+            req.setAttribute("info", "New assignment is added!");
+        } catch (SQLException e) {
+            throw new ServletException();
+        } catch (ServiceException e) {
+            req.setAttribute("error", e.getMessage());
         }
+        req.getRequestDispatcher("newassignment.jsp").forward(req, resp);
     }
 }
