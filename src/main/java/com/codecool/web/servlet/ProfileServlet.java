6cf026408ab793ea81e5920f8ef628a745655cb5
Bernáth Máté <bernath.mate.new@gmail.com>
2018-04-19 09:47:36 +0200

    doGet method is fixed, this method will send the current user to the jsp from the DB

--- src/main/java/com/codecool/web/servlet/ProfileServlet.java
+++ src/main/java/com/codecool/web/servlet/ProfileServlet.java
@@ -1,5 +1,7 @@
 package com.codecool.web.servlet;
 
+import com.codecool.web.dao.UserDao;
+import com.codecool.web.dao.database.DatabaseUserDao;
 import com.codecool.web.model.User;
 
 import javax.servlet.ServletException;
@@ -8,12 +10,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @WebServlet("/protected/profile")
 public class ProfileServlet extends AbstractServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User user = (User) req.getSession().getAttribute("user");
-        req.setAttribute("user", user);
-        req.getRequestDispatcher("profile.jsp").forward(req, resp);
+        User sessionUser = (User) req.getSession().getAttribute("user");
+        try (Connection connection = getConnection(req.getServletContext())) {
+            UserDao userDao = new DatabaseUserDao(connection);
+            User user = userDao.getUserByEmail(sessionUser.getEmail());
+            req.setAttribute("user", user);
+            req.getRequestDispatcher("profile.jsp").forward(req, resp);
+        } catch (SQLException e) {
+            throw new ServletException();
+        }
     }
 }
